842483664d9807a0a362c510dcd9a85c79a968f1
SokolovPE <SokolovP@outlook.com>
2021-03-31 04:54:49 +0300

    add todo for cart service

--- src/store/modules/cart.module.js
+++ src/store/modules/cart.module.js
@@ -14,10 +14,17 @@ const mutations = {
     DEL_ITEM(state, payload) {
         state.items.filter((item) => item.id !== payload);
     },
+    SET_ITEMS(state, payload) {
+        state.items = payload;
+    }
 };
 
 const actions = {
     addItem: ({ commit }, payload) => {
+        // TODO: add service to store cart at server.
+        // CartService.addItem(payload).then((response) => {
+        //     commit('SET_ITEMS', response.data);
+        // });
         commit('ADD_ITEM', payload);
     },
     delItem: ({ commit }, payload) => {
